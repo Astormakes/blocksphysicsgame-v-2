8d9f18926bb27664083197446fbff1e30b0791e1
Astormakes <astor111097@gmail.com>
2025-11-22 21:54:55 +0100

    added fps couter back also made it where mouswheel can change the currently selected item.

--- Scenes/Player/PlayerHead.gd
+++ Scenes/Player/PlayerHead.gd
@@ -6,9 +6,14 @@ var action
 var action2
 
 @onready var player:Node3D = $"../.."
+@onready var lable = $Control/Label
+@onready var fps = $"Control/fps counter"
 
 @onready var camera = $Camera3D
 # Called when the node enters the scene tree for the first time.
+
+var item:int
+
 func _ready() -> void:
 	if is_multiplayer_authority():
 		$Camera3D.current = true
@@ -19,6 +24,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	fps.text = str(Engine.get_frames_per_second())
 	
 	var mouse_pos = get_viewport().get_mouse_position()
 	ray.target_position = to_local(camera.project_ray_normal(mouse_pos)*2 + camera.project_ray_origin(mouse_pos))
@@ -27,9 +33,16 @@ func _input(_event: InputEvent) -> void:
 	if not is_multiplayer_authority(): return
 	if not camera.current: return
 	
+	if Input.is_action_just_released("mousewheel_up"):
+		item = clamp(item+1,0,99)
+		lable.text = str(item)
+		
+	if Input.is_action_just_released("mousewheel_down"):
+		item = clamp(item-1,0,99)
+		lable.text = str(item)
 	
-	var action = "mouse1"
-	var action2 = "_pressed"
+	action = "mouse1"
+	action2 = "_pressed"
 	if Input.is_action_just_pressed(action):
 		var obj = ray.get_collider()
 		if obj:
